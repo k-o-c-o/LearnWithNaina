86a3a850b91498bbbeb56568276ec8a4083e5eed
Naina Edwin <naina202404@gmail.com>
2026-06-22 15:24:05 +0530

    Implemented User authentication

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,33 @@
+//imports
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+//load env variables
+dotenv.config();
+
+//connect to MongoDB
+connectDB();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-const PORT = 5000;
-
+//Home route
 app.get("/",(req,res)=>{
     res.json({
         message: "Learn with Naina backend"
     });
 });
 
+//Sets port
+const PORT = process.env.PORT || 5000;
+
+//start seerver
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
